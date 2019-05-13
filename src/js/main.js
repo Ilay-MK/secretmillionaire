284f4b2de173239e7ecef3fe748ccab8af6c50e3
webdevsynapse <webdevsynapse@gmail.com>
2019-05-13 14:44:54 +0300

    Initialized swiper

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,20 @@
 "use strict";
+
+// Swiper initializing
+$(document).ready(function () {
+	//initialize swiper when document ready
+	/*var mySwiper = new Swiper('.swiper-container', {
+		// Optional parameters
+		direction: 'vertical',
+		loop: true
+	})*/
+
+	var swiper = new Swiper('.swiper-container', {
+		slidesPerView: 3,
+		spaceBetween: 30,
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		},
+	});
+});
