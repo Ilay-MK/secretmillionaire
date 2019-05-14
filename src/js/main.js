33220bffa22101aeb52dccc0e9db747e1a631040
webdevsynapse <webdevsynapse@gmail.com>
2019-05-14 06:16:16 +0300

    Animation sliders

--- src/js/main.js
+++ src/js/main.js
@@ -8,11 +8,27 @@ $(document).ready(function () {
 		direction: 'vertical',
 		loop: true
 	})*/
-
-	var swiper = new Swiper('.slider_women .swiper-container', {
+	var swiperWomen = new Swiper('.slider_women .swiper-container', {
 		/*slidesPerView: 2,*/
-		slidesPerView: 'auto',
+		slidesPerView: '3',
 		spaceBetween: 0,
+		slidesPerGroup: 1,
+		// Responsive breakpoints
+		breakpoints: {
+			// when window width is <= 320px
+			1023.98: {
+				slidesPerView: 1,
+				slidesPerGroup: 1
+			},
+			// when window width is <= 480px
+			1439.98: {
+				slidesPerView: 2,
+				slidesPerGroup: 2
+			}
+		},
+		speed: 900,
+		/*loop: true,*/
+		/*loopFillGroupWithBlank: true,*/
 		/*effect: 'fade',*/
 		/*pagination: {
 			el: '.swiper-pagination',
@@ -24,10 +40,28 @@ $(document).ready(function () {
 		},
 	});
 
-	var swiper = new Swiper('.slider_building .swiper-container', {
-		/*slidesPerView: 1,*/
-		slidesPerView: 'auto',
-		spaceBetween: 8,
+	swiperWomen.on('slideChangeTransitionStart', function () {
+		//$('.slider_women .swiper-slide').css("opacity", ".3");
+		if ($('.slider_women .swiper-slide').hasClass("inverse")) {
+			$('.slider_women .swiper-slide').removeClass("inverse");
+		} else {
+			$('.slider_women .swiper-slide').addClass("inverse");
+		}
+	});
+	swiperWomen.on('slideChangeTransitionEnd', function () {
+		/*$('.slider_women .swiper-slide').css("opacity", "1");*/
+	});
+
+	var swiperBuilding = new Swiper('.slider_building .swiper-container', {
+		slidesPerView: '2',
+		spaceBetween: 25,
+		//autoHeight: true, //enable auto height
+		/*slidesPerGroup: 2,*/
+		breakpoints: {
+			1439.98: {
+				slidesPerView: 'auto'
+			}
+		},
 		/*effect: 'fade',*/
 		/*pagination: {
 			el: '.swiper-pagination',
